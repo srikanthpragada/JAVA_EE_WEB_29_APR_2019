87f05d1447bf0bee75c0d1bc020ed8d2c81e23f4
srikanthpragada <srikanthpragada@gmail.com>
2019-05-11 15:57:28 +0530

    11-MAY

---
+++ src/servlets/NowServlet.java
@@ -0,0 +1,25 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/now" })
+public class NowServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, 
+			             HttpServletResponse response) throws ServletException, IOException {
+
+		  PrintWriter writer = response.getWriter();
+		  response.setContentType("text/html");
+		  LocalDateTime ldt = LocalDateTime.now();
+		  writer.println("<h2>" + ldt.toString() + "</h2>");
+		  
+	}
+
+}
